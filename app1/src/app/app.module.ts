76dc63a5c494b9b0b8ee2ea76f2ad0bce786dabc
Vinicius <v1n1c1u5_1@hotmail.com>
2019-10-19 15:21:05 -0300

    merge

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
+import { TentativaComponent } from './tentativa/tentativa.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent,
+    TentativaComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule,
---
+++ app1/src/app/tentativa/tentativa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tentativa',
+  templateUrl: './tentativa.component.html',
+  styleUrls: ['./tentativa.component.css']
+})
+export class TentativaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
