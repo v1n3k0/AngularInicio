1f36c282ac4f671892e008b924319774e4a95444
Vinicius <v1n1c1u5_1@hotmail.com>
2019-10-20 13:02:28 -0300

    Aula 55

---
+++ app1/src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model';
+
+const FRASES: Array<Frase> = [
+    { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
+    { fraseEng: 'I watch tv', frasePtBr: 'Eu assisto tv' },
+    { fraseEng: 'How are you?', frasePtBr: 'Como vai você' },
+    { fraseEng: 'I eat bread', frasePtBr: 'EU como pão' }
+];
