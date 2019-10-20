d1a74e9addd60400cc6c62da326674a00ced3ad3
Vinicius <v1n1c1u5_1@hotmail.com>
2019-10-20 20:15:19 -0300

    Aula 82

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -8,9 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   public jogoEmAndamento = true;
+  public tipoEncerramento: string;
 
   public encerrarJogo(tipo: string): void {
-    console.log(tipo);
     this.jogoEmAndamento = false;
+    this.tipoEncerramento = tipo;
+  }
+
+  public reiniciarJogo(): void {
+    this.jogoEmAndamento = true;
+    this.tipoEncerramento = undefined;
   }
 }
