1e446024425573661d1151a18025d676635c8942
Vinicius <v1n1c1u5_1@hotmail.com>
2019-10-27 20:46:06 -0300

    Aula 110

--- app2/src/app/ofertas.services.ts
+++ app2/src/app/ofertas.services.ts
@@ -8,7 +8,7 @@ export class OfertasService {
     constructor(private http: HttpClient) { }
 
     public getOfertas() {
-        return this.http.get<Oferta[]>('http://localhost:3000/ofertas')
+        return this.http.get<Oferta[]>('http://localhost:3000/ofertas?destaque=true')
         .toPromise()
         .then((res: any) => res);
     }
