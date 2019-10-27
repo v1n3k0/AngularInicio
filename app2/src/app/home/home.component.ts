62b5906be55c957e8ec57a07a76695fb36ad93d9
Vinicius <v1n1c1u5_1@hotmail.com>
2019-10-26 23:53:38 -0300

    Aula 98

--- app2/src/app/home/home.component.ts
+++ app2/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.services';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[];
+
+  constructor(private ofertasServices: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.ofertasServices.getOfertas();
+
+    console.log(this.ofertas);
   }
 
 }
---
+++ app2/src/app/shared/oferta.model.ts
@@ -0,0 +1,10 @@
+export class Oferta {
+    id: number;
+    categoria: string;
+    titulo: string;
+    descricaoOferta: string;
+    anunciante: string;
+    valor: number;
+    destaque: boolean;
+    imagens: Array<object>;
+}
