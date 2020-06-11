12b067eec80baf7457ee8bafa16c0c7214e52280
Vinicius <v1n1c1u5_1@hotmail.com>
2020-06-11 11:11:53 -0300

    Aula 165

--- app2/src/app/app.module.ts
+++ app2/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { ROUTES } from './app.routes';
@@ -14,6 +14,8 @@ import { from } from 'rxjs';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,9 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     HttpClientModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+registerLocaleData(localePt, 'pt');
