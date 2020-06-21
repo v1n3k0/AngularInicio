6e13ed6ff88d821e56950539fe5d0952967e82fc
Vinicius <v1n1c1u5_1@hotmail.com>
2020-06-21 18:27:54 -0300

    Aula 191

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.services';
-import { Pedido } from '../shared/pedido.model';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -10,91 +9,9 @@ import { Pedido } from '../shared/pedido.model';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  public idPedidoCompra: number;
-
-  public pedido: Pedido = new Pedido('', '', '', '');
-
-  public endereco = '';
-  public numero = '';
-  public complemento = '';
-  public formaPagamento = '';
-
-  public enderecoValido: boolean;
-  public numeroValido: boolean;
-  public complementoValido: boolean;
-  public formaPagamentoValido: boolean;
-
-  public enderecoEstadoPrimitivo = true;
-  public numeroEstadoPrimitivo = true;
-  public complementoEstadoPrimitivo = true;
-  public formaPagamentoEstadoPrimitivo = true;
-
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
-    // this.ordemCompraService.efetivarCompra();
-  }
 
-  public atualizaEndereco(endereco: string) {
-    this.endereco = endereco;
-    // console.log(this.endereco);
-
-    this.enderecoEstadoPrimitivo = false;
-
-    if (this.endereco.length > 3) {
-      this.enderecoValido = true;
-    } else {
-      this.enderecoValido = false;
-    }
-  }
-
-  public atualizaNumero(numero: string) {
-    this.numero = numero;
-    // console.log(this.numero);
-
-    this.numeroEstadoPrimitivo = false;
-
-    if (this.numero.length > 0) {
-      this.numeroValido = true;
-    } else {
-      this.numeroValido = false;
-    }
-  }
-
-  public atualizaComplemento(complemento: string) {
-    this.complemento = complemento;
-    // console.log(this.complemento);
-
-    this.complementoEstadoPrimitivo = false;
-
-    if (this.complemento.length > 0) {
-      this.complementoValido = true;
-    } else {
-      this.complementoValido = false;
-    }
-  }
-
-  public atualizaFormaPagamento(formaPagamento: string) {
-    this.formaPagamento = formaPagamento;
-    // console.log(this.formaPagamento);
-
-    this.formaPagamentoEstadoPrimitivo = false;
-
-    if (this.formaPagamento.length > 0) {
-      this.formaPagamentoValido = true;
-    } else {
-      this.formaPagamentoValido = false;
-    }
-  }
-
-  public confirmarCompra() {
-    this.pedido.endereco = this.endereco;
-    this.pedido.numero = this.numero;
-    this.pedido.complemento = this.complemento;
-    this.pedido.formaPagamento = this.formaPagamento;
-
-    this.ordemCompraService.efetivarCompra(this.pedido).subscribe((idPedido: number) => {
-      this.idPedidoCompra = idPedido;
-    });
   }
 }
