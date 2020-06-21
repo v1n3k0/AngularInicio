913166d724c102ad3c12343c9f7b524758e6a1f5
Vinicius <v1n1c1u5_1@hotmail.com>
2020-06-20 23:46:21 -0300

    Aula 179

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -12,6 +12,11 @@ export class OrdemCompraComponent implements OnInit {
   public complemento = '';
   public formaPagamento = '';
 
+  public enderecoValido: boolean;
+  public numeroValido: boolean;
+  public complementoValido: boolean;
+  public formaPagamentoValido: boolean;
+
   constructor() { }
 
   ngOnInit() {
@@ -19,21 +24,45 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string) {
     this.endereco = endereco;
-    console.log(this.endereco);
+    // console.log(this.endereco);
+
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
   }
 
   public atualizaNumero(numero: string) {
     this.numero = numero;
-    console.log(this.numero);
+    // console.log(this.numero);
+
+    if (this.numero.length > 0) {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
   }
 
   public atualizaComplemento(complemento: string) {
     this.complemento = complemento;
-    console.log(this.complemento);
+    // console.log(this.complemento);
+
+    if (this.complemento.length > 0) {
+      this.complementoValido = true;
+    } else {
+      this.complementoValido = false;
+    }
   }
 
   public atualizaFormaPagamento(formaPagamento: string) {
     this.formaPagamento = formaPagamento;
-    console.log(this.formaPagamento);
+    // console.log(this.formaPagamento);
+
+    if (this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
   }
 }
