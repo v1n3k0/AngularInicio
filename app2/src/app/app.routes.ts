0983ed6e3d0a5ce9fa21bea8cdad4007829d67e8
Vinicius <v1n1c1u5_1@hotmail.com>
2020-06-20 21:23:02 -0300

    Aula 174

--- app2/src/app/app.routes.ts
+++ app2/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
@@ -19,4 +20,5 @@ export const ROUTES: Routes = [
             { path: 'onde-fica/:id', component: OndeFicaComponent },
         ]
     },
+    { path: 'ordem-compra', component: OrdemCompraComponent}
 ];
