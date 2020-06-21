e23d49596999169ba06d5d8b8e896d35196fb22f
Vinicius <v1n1c1u5_1@hotmail.com>
2020-06-20 23:27:33 -0300

    Aula 177

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco = '';
+  public numero = '';
+  public complemento = '';
+  public formaPagamento = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public atualizaEndereco(endereco: string) {
+    this.endereco = endereco;
+    console.log(this.endereco);
+  }
+
+  public atualizaNumero(numero: string) {
+    this.numero = numero;
+    console.log(this.numero);
+  }
+
+  public atualizaComplemento(complemento: string) {
+    this.complemento = complemento;
+    console.log(this.complemento);
+  }
+
+  public atualizaFormaPagamento(formaPagamento: string) {
+    this.formaPagamento = formaPagamento;
+    console.log(this.formaPagamento);
+  }
 }
