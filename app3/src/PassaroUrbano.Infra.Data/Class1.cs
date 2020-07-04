431363b40bac202069904ad395b028a02e46dc3c
Vinicius <v1n1c1u5_1@hotmail.com>
2020-07-04 11:41:16 -0300

    inicio

---
+++ app3/src/PassaroUrbano.Infra.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PassaroUrbano.Infra.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ app3/src/PassaroUrbano.Infra.IoC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PassaroUrbano.Infra.IoC
+{
+    public class Class1
+    {
+    }
+}
