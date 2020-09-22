2eafd048f936523e8fd5cf8e824333b9a1f19216
Vinicius <v1n1c1u5_1@hotmail.com>
2020-09-22 17:18:41 -0300

    Inicio desenvolvimento de teste

---
+++ app3/test/PassaroUrbano.Test/Oferta/OfertaTeste.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace PassaroUrbano.Test.Oferta
+{
+    public class OfertaTeste
+    {
+        [Fact(DisplayName = "Testar")]
+        public void Testar()
+        {
+            var var1 = 1;
+            var var2 = 1;
+
+            Assert.Equal(var1, var2);
+        }
+    }
+}
